e9759935a7abd37b4802f25ea86910ddf4f4060e
Aldo Daniel Lara Emiliano <106570943+aldodanielle@users.noreply.github.com>
2024-01-24 11:06:23 -0600

    Revert "Actualizacion y listo´"

--- js/comprobacion.js
+++ js/comprobacion.js
@@ -1,8 +1,1 @@
-let repuesta;
-
-console.log("¿Seria todo de su orden?");
-if(repuesta === true){
-    console.log("Ok en un momento estara su orden")
-}else{
-    console.log("¿Que mas le gustaria ordenar?");
-}
+console.log("Comprobar orden");
