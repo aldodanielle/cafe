0bb0937f0373d04a171d8e7947acf699de84ae09
Aldo Daniel <aldo.lara@coppel.com>
2024-01-29 13:00:43 -0600

    Update of the file comprobacion.js

--- js/comprobacion.js
+++ js/comprobacion.js
@@ -1,1 +1,12 @@
 console.log("Comprobar orden");
+
+let respuesta;
+console.log("¿Seria todo en su orden?");
+respuesta = false;
+
+if(respuesta === true){
+    console.log("OK");
+    console.log("En un momento esta su pedido");
+}else{
+    console.log("¿Que mas le gustaria ordena?");
+}
